52407b6feb49693535d63494d7dc6d97314879b9
1br0h1m-cyber <normahmatovibrohim0409@gmail.com>
2025-03-26 13:22:22 +0500

    card update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,11 +6,9 @@ import { FaCalendarDay } from "react-icons/fa6";
 import { IoBagSharp } from 'react-icons/io5'
 import { FaClock } from "react-icons/fa6";
 
-const Card = () => {
+const Card = ({title, desc, icon, color,}) => {
 
     const Months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-
-    const date = new Date()
 
     const [time, setTime] = useState(new Date());
 
@@ -22,51 +20,18 @@ const Card = () => {
       return () => clearInterval(interval);
     }, []);
 
+    const day = `${Months[time.getMonth()]} ${time.getDate()}, ${time.getFullYear()}`
+
     return (
-        <div className='flex flex-col md:grid md:grid-cols-2 lg:grid-cols-4 h-auto gap-3 py-3'>
-
-            <div className='flex bg-white rounded-xl shadow-md gap-2 p-4 h-full w-full'>
-                <div className='bg-blue-500 text-white h-min w-min rounded-full m-1 p-2.5'>
-                    <IoBagSharp size={22} />
-                </div>
-                <div>
-                    <h2 className='text-[16px] text-gray-400'>Total Products</h2>
-                    <h1 className='font-bold'>124</h1>
-                </div>
+        <div className='flex bg-white rounded-xl shadow-md gap-2 p-4 h-full w-full'>
+            <div className={`bg-${color} text-white h-min w-min rounded-full m-1 p-2.5`}>
+                {icon}
             </div>
-
-            <div className='flex bg-white rounded-xl shadow-md gap-2 p-4 h-full w-full'>
-                <div className='bg-green-500 text-white h-min w-min rounded-full m-1 p-2.5'>
-                    <FaUserFriends size={22} />
-                </div>
-                <div>
-                    <h2 className='text-[16px] text-gray-400'>Total Admins</h2>
-                    <h1 className='font-bold'>8</h1>
-                </div>
+            <div>
+                <h2 className='text-[16px] text-gray-400'>{title}</h2>
+                <h1 className='font-bold'>{desc}</h1>
             </div>
-
-            <div className='flex bg-white rounded-xl shadow-md gap-2 p-4 h-full w-full'>
-                <div className='bg-orange-400 text-white h-min w-min rounded-full m-1 p-2.5'>
-                    <FaCalendarDay size={22} />
-                </div>
-                <div>
-                    <h2 className='text-[16px] text-gray-400'>Today's date</h2>
-                    <h1 className='font-bold'>{`${Months[time.getMonth()]} ${time.getDate()}, ${time.getFullYear()}`}</h1>
-                </div>
-            </div>
-
-            <div className='flex bg-white rounded-xl shadow-md gap-2 p-4 h-full w-full'>
-                <div className='bg-violet-500 text-white h-min w-min rounded-full m-1 p-2.5'>
-                    <FaClock size={22} />
-                </div>
-                <div>
-                    <h2 className='text-[16px] text-gray-400'>Total Products</h2>
-                    <h1 className='font-bold'>{time.toLocaleTimeString()}</h1>
-                </div>
-            </div>
-
         </div>
-
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+//components
 import Card from '../components/Card'
+//icons
+import { FaUserFriends } from 'react-icons/fa'
+import { FaCalendarDay } from "react-icons/fa6";
+import { IoBagSharp } from 'react-icons/io5'
+import { FaClock } from "react-icons/fa6";
 
 const Home = () => {
 
-  const [isAuth,setIsAuth] = useState(true)
-  const navigate = useNavigate()
+    const Months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
-  useEffect(()=>{
-    if(!isAuth){
-      navigate('/login')
-    }
-  },[])
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setTime(new Date());
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }, []);
+
+    const day = `${Months[time.getMonth()]} ${time.getDate()}, ${time.getFullYear()}`
 
   return (
     <div className='p-4'>
@@ -19,8 +30,11 @@ const Home = () => {
         <h1 className='font-bold text-2xl'>Welcome to the Admin Dashboard</h1>
         <h2 className='text-[20px]'>This is your central hub for managing your application. Use the navigation menu to access different sections.</h2>
       </div>
-      <div className='w-full'>
-        <Card/>
+      <div className='flex flex-col md:grid md:grid-cols-2 lg:grid-cols-4 h-auto gap-3 py-3 w-full'>
+        <Card title={'Total Products'} desc={124} color={'blue-500'} icon={<IoBagSharp size={22} />}/>
+        <Card title={'Total Admins'} desc={8} color={'green-500'} icon={<FaUserFriends size={22} />}/>
+        <Card title={"Today's date"} desc={day} color={'orange-400'} icon={<FaCalendarDay size={22} />}/>
+        <Card title={'Current time'} desc={time.toLocaleTimeString()} color={'violet-500'} icon={<FaClock size={22} />}/>
       </div>
     </div>
   )
